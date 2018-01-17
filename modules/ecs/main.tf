c8ca44155e45e82dc2faa5278e96d3019228f472
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-01-18 10:21:28 +1100

    Generalize naming in ecs module

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,5 +1,5 @@
 
-resource "aws_ecs_task_definition" "datacube-service-task" {
+resource "aws_ecs_task_definition" "task_definition" {
   family = "${var.family}"
   container_definitions = "${var.container_definitions}"
   task_role_arn = "${var.task_role_arn}"
@@ -10,10 +10,10 @@ resource "aws_ecs_task_definition" "datacube-service-task" {
 }
 
 
-resource "aws_ecs_service" "datacube-service" {
+resource "aws_ecs_service" "service_definition" {
   name = "${var.name}"
   cluster = "${var.cluster}"
-  task_definition = "${aws_ecs_task_definition.datacube-service-task.arn}"
+  task_definition = "${aws_ecs_task_definition.task_definition.arn}"
   desired_count = "${var.desired_count}"
   load_balancer {
     target_group_arn = "${var.target_group_arn}"

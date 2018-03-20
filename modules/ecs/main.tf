6919388cb91793756259bf34cfc394b5357300d4
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-03-20 17:04:08 +1100

    Remove reference to datacube

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,5 +1,5 @@
 
-resource "aws_ecs_task_definition" "datacube-service-task" {
+resource "aws_ecs_task_definition" "service-task" {
   family = "${var.family}"
   container_definitions = "${var.container_definitions}"
   task_role_arn = "${var.task_role_arn}"
@@ -13,10 +13,10 @@ resource "aws_ecs_task_definition" "datacube-service-task" {
 }
 
 
-resource "aws_ecs_service" "datacube-service" {
+resource "aws_ecs_service" "service" {
   name = "${var.name}"
   cluster = "${var.cluster}"
-  task_definition = "${aws_ecs_task_definition.datacube-service-task.arn}"
+  task_definition = "${aws_ecs_task_definition.service-task.arn}"
   desired_count = "${var.desired_count}"
   load_balancer {
     target_group_arn = "${var.target_group_arn}"

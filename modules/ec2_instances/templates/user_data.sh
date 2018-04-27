3934ce1b50d84a984cb49c897803183baaea5571
tom-butler <tombutler.development@gmail.com>
2018-04-27 14:31:35 +1000

    fix logging drivers (#33)

--- modules/ec2_instances/templates/user_data.sh
+++ modules/ec2_instances/templates/user_data.sh
@@ -11,7 +11,8 @@ yum install -y awslogs jq aws-cli nfs-utils
 ${ecs_config}
 {
   echo "ECS_CLUSTER=${cluster_name}"
-  echo "ECS_AVAILABLE_LOGGING_DRIVERS=${ecs_logging}"
+  # Has to be single quote because we're injecting double quotes
+  echo 'ECS_AVAILABLE_LOGGING_DRIVERS=${ecs_logging}'
 } >> /etc/ecs/ecs.config
 
 # Inject the CloudWatch Logs configuration file contents

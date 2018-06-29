9c177622b811e80f5a7bc639fbe6b6d875a48938
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-06-29 14:12:17 +1000

    Do not run deploy/dry-run if workspace directory does not exist

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@ echo "Deploying $1"
 rm -rf .terraform
 export WORKSPACE=$1
 export AWS_PROFILE="$2"
+if [ ! -d "workspaces/$WORKSPACE" ]
+then
+    echo "Could not find workspace directory $WORKSPACE"
+    echo "Aborting..."
+    exit 1
+fi
 terraform init -backend-config workspaces/$WORKSPACE/backend.cfg
 terraform workspace new $WORKSPACE || terraform workspace select $WORKSPACE
 terraform plan -input=false -var-file="workspaces/$WORKSPACE/terraform.tfvars"
